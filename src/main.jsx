b5606c2db80dcd5ec259a53f1cbfdc6bbe067d3b
Jihanchy <habibullahalquaderi2005@gmail.com>
2024-12-29 15:05:19 +0600

    added banner

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { router } from './Routes/Router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <div  className="max-w-screen-xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
